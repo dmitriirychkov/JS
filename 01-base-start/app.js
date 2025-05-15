3ca5ed4b3fa8ca6c2cc15e535972553012913082
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-15 14:40:49 +0500

    Update app.js

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -2,5 +2,16 @@ let num = 37
 let firstName = 'Dmitrii'
 const isProgrammer = true 
 
-firstName = 'Tomyshev'
-isProgrammer = false //error
+// Can Do
+// let $ = 'test'
+// let $num = 37
+// let num$ = 37
+// let _ = 49
+// let _num = 12
+// let num_ = 12
+// let first_name = 'Eva' // bad
+// let myNum = 37 // good
+// let num42 = 10
+
+// firstName = 'Tomyshev'
+// isProgrammer = false // error because of const
