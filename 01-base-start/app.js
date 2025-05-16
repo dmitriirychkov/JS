673d99ed445d27e0a10b74f6032c35fe25d8b593
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-16 14:43:38 +0500

    Update app.js

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -66,23 +66,22 @@ minusBtn.onclick = function () {
     action = '-'
 }
 
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'    
+    }
+    resultElement.textContent = result
+}
+
 submitBtn.onclick = function () {
 
     if (action == '+') {
         const sum = Number(input1.value) + Number(input2.value)
-        if (sum < 0) {
-            resultElement.style.color = 'red'
-        } else {
-            resultElement.style.color = 'green'    
-        }
-        resultElement.textContent = sum
+        printResult(sum)
     } else if (action == '-') {
         const sum = Number(input1.value) - Number(input2.value)
-        if (sum < 0) {
-            resultElement.style.color = 'red'
-        } else {
-            resultElement.style.color = 'green'    
-        }
-        resultElement.textContent = sum
+        printResult(sum)
     }
 }
