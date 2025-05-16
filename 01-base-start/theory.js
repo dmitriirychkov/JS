4699d7a3b77235b238b84d98614218c840da5a8b
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-16 17:22:02 +0500

    Update theory.js

--- 01-base-start/theory.js
+++ 01-base-start/theory.js
@@ -31,4 +31,9 @@ const currentYear = 2025
 // > < >= <=
 const isFullAge = currentYear - birthYear >= fullAge
 
-console.log(isFullAge)
+// console.log(isFullAge)
+
+const num1 = 42
+const num2 = '42'
+
+console.log(num1 === num2)
