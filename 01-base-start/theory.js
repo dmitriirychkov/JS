18673eaa95a295e540241f1b90d1ab64b7fea4d7
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-16 17:18:59 +0500

    Update theory.js

--- 01-base-start/theory.js
+++ 01-base-start/theory.js
@@ -15,9 +15,20 @@ let c = 32
 
 // c = 4 ** 2
 
-// =============== Data Types
+// ================ Data Types
 const age = 29 // number
 const name = 'Dmitrii' // string
 const isProgrammer = true // boollean 
 let x // undefined 
-console.log(typeof name)
+// null
+// console.log(typeof (1 / x))
+// console.log(typeof NaN)
+
+// ================ Priority
+const fullAge = 29
+const birthYear = 1998
+const currentYear = 2025
+// > < >= <=
+const isFullAge = currentYear - birthYear >= fullAge
+
+console.log(isFullAge)
