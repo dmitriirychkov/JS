9fb3e5b18f33233a52f0c13c174f800f29223dc4
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-16 14:56:24 +0500

    Update app.js

--- 01-base-start/app.js
+++ 01-base-start/app.js
@@ -75,13 +75,27 @@ function printResult(result) {
     resultElement.textContent = result
 }
 
-submitBtn.onclick = function () {
-
-    if (action == '+') {
-        const sum = Number(input1.value) + Number(input2.value)
-        printResult(sum)
-    } else if (action == '-') {
-        const sum = Number(input1.value) - Number(input2.value)
-        printResult(sum)
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    if (actionSymbol == '+') {
+        return num1 + num2
     }
+
+    if (actionSymbol == '-') {
+        return num1 - num2
+    }
+}
+
+submitBtn.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+    
+    // if (action == '+') {
+    //    const sum = Number(input1.value) + Number(input2.value)
+    //    printResult(sum)
+    // } else if (action == '-') {
+    //    const sum = Number(input1.value) - Number(input2.value)
+    //    printResult(sum)
+    // }
 }
