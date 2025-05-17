aa95b5836ffab8038d554934d9eb155f1f1a5e9b
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 16:53:52 +0500

    Update app.js

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -4,4 +4,5 @@ const array = [1, 2, 3, 5, 20, 42]
 // const arrayStrings = ['a', 'b', 'c', null, 12]
 // const array = new Array(1, 2, 3, 5, 20, 42)
 
-console.log(array.length)
+// console.log(array.length)
+console.log(array[1])
