8e922b579e468ceb0c345bc5411b72172a558ffb
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 16:57:58 +0500

    Update app.js
    array[0] = 'Hello!'
    console.log(array)

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -6,4 +6,6 @@ const array = [1, 2, 3, 5, 20, 42]
 
 // console.log(array.length)
 console.log(array[1])
-console.log(array[array.length - 1]) // array[6]
+console.log(array[array.length - 1]) // array[6 -1]
+array[0] = 'Hello!'
+console.log(array)
