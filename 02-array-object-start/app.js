8cddf6d7c0e8fab01ac3a4c8586a235e79ec8660
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 17:31:07 +0500

    Update app.js
        listElement.insertAdjacentHTML('beforeend', `
                <li
                    class="list-group-item d-flex justify-content-between align-items-center"
                >
                    <span>${inputElement.value}</span>
                    <span>
                    <span class="btn btn-small btn-success">&check;</span>
                    <span class="btn btn-small btn-danger">&times;</span>
                    </span>
                </li>
                `)
    }

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -18,15 +18,17 @@ const listElement = document.getElementById('list')
 // console.log(inputElement.value)
 
 createBtn.onclick = function () {
-    listElement.innerHTML = `
-    <li
-        class="list-group-item d-flex justify-content-between align-items-center"
-    >
-        <span>${inputElement.value}</span>
-        <span>
-          <span class="btn btn-small btn-success">&check;</span>
-          <span class="btn btn-small btn-danger">&times;</span>
-        </span>
-    </li>
-    `
+    // listElement.innerHTML = 
+
+    listElement.insertAdjacentHTML('beforeend', `
+            <li
+                class="list-group-item d-flex justify-content-between align-items-center"
+            >
+                <span>${inputElement.value}</span>
+                <span>
+                <span class="btn btn-small btn-success">&check;</span>
+                <span class="btn btn-small btn-danger">&times;</span>
+                </span>
+            </li>
+            `)
 }
