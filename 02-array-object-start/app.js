c5d1505218256fc0a499b62beb242ad9c2019683
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 17:34:14 +0500

    Update app.js
    if (inputElement.value.length === 0) {
        return
    }

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -18,8 +18,10 @@ const listElement = document.getElementById('list')
 // console.log(inputElement.value)
 
 createBtn.onclick = function () {
+if (inputElement.value.length === 0) {
+    return
+}
     // listElement.innerHTML = 
-
     listElement.insertAdjacentHTML('beforeend', `
             <li
                 class="list-group-item d-flex justify-content-between align-items-center"
