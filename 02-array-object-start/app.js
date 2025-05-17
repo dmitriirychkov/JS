213e9c5deb0f252d058f73b8da318fadd4c5f4f5
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 17:17:56 +0500

    Update app.js
    createBtn.onclick = function () {
        listElement.innerHTML = `
        <li
            class="list-group-item d-flex justify-content-between align-items-center"
        >
            <span>Название</span>
            <span>
              <span class="btn btn-small btn-success">&check;</span>
              <span class="btn btn-small btn-danger">&times;</span>
            </span>
        </li>
        `
    }

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -18,5 +18,15 @@ const listElement = document.getElementById('list')
 // console.log(inputElement.value)
 
 createBtn.onclick = function () {
-    listElement.innerHTML = inputElement.value
+    listElement.innerHTML = `
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    >
+        <span>Название</span>
+        <span>
+          <span class="btn btn-small btn-success">&check;</span>
+          <span class="btn btn-small btn-danger">&times;</span>
+        </span>
+    </li>
+    `
 }
