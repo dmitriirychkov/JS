997a72d00ea26752992939f8b09165a61c1a3063
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-17 17:07:47 +0500

    Update app.js
    createBtn.onclick = function

--- 02-array-object-start/app.js
+++ 02-array-object-start/app.js
@@ -1,12 +1,22 @@
 // Theory
 
-const array = [1, 2, 3, 5, 20, 42]
+// const array = [1, 2, 3, 5, 20, 42]
 // const arrayStrings = ['a', 'b', 'c', null, 12]
 // const array = new Array(1, 2, 3, 5, 20, 42)
 
 // console.log(array.length)
-console.log(array[1])
-console.log(array[array.length - 1]) // array[6 -1]
-array[0] = 'Hello!'
-console.log(array)
-array[array.length] = 'becon'
+// console.log(array[1])
+// console.log(array[array.length - 1]) // array[6 -1]
+// array[0] = 'Hello!'
+// console.log(array)
+// array[array.length] = 'becon'
+
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+
+// console.log(inputElement.value)
+
+createBtn.onclick = function () {
+    
+}
