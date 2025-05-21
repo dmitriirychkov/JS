06b73ed71f3af4051d01d9b69d09efe6578aad22
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-21 15:48:21 +0500

    Update 02-strings.js
    console.log(firstName.length)
    console.log(firstName)
    console.log(firstName.toUpperCase())
    console.log(firstName.toLowerCase())
    console.log(firstName.charAt(6))
    console.log(firstName.indexOf('!'))
    console.log(firstName.startsWith('Dmi'))
    console.log(firstName.toLowerCase().startsWith('rii'))
    console.log(firstName.endsWith('rii'))
    console.log(firstName.repeat(3))
    
    const password = '    my super pass    '
    
    console.log(password.trim())

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/02-strings.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/02-strings.js
@@ -1,4 +1,4 @@
-const name = 'Dmitrii'
+const firstName = 'Dmitrii'
 const age = 37
 
 function getAge() {
@@ -6,6 +6,22 @@ function getAge() {
 }
 
 // const old = 'Hello my name is ' + name + ' and my age is' + age
-const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? 'Cant drive' : `Can't drive`}`
+// const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? 'Cant drive' : `Can't drive`}`
 
-console.log(output)
+// console.log(output)
+
+console.log(firstName.length)
+console.log(firstName)
+console.log(firstName.toUpperCase())
+console.log(firstName.toLowerCase())
+console.log(firstName.charAt(6))
+console.log(firstName.indexOf('!'))
+console.log(firstName.startsWith('Dmi'))
+console.log(firstName.toLowerCase().startsWith('rii'))
+console.log(firstName.endsWith('rii'))
+console.log(firstName.repeat(3))
+
+const password = '    my super pass    '
+
+console.log(password.trim())
+
