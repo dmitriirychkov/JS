e46305e80a6d2ee98c693cad0e619425b232a7bb
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-21 15:32:14 +0500

    Update 02-strings.js
    const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? 'Cant drive' : `Can't drive`}`

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/02-strings.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/02-strings.js
@@ -1,7 +1,11 @@
 const name = 'Dmitrii'
 const age = 37
 
+function getAge() {
+    return age
+}
+
 // const old = 'Hello my name is ' + name + ' and my age is' + age
-const output = `Hello my name is ${name} and my age is ${age}`
+const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? 'Cant drive' : `Can't drive`}`
 
 console.log(output)
