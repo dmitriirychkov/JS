c2bec22d30ce80bfd282c2fd761a66e892aa8e09
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-22 15:48:52 +0500

    Update 03-functions.js
    Function Declaration and Function Expression

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/03-functions.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/03-functions.js
@@ -3,5 +3,12 @@ function greet(name) {
     console.log('Hello - ', name)
 }
 
+// Function Expression
+const greet2 = function (name) {
+    console.log('2 Hello - ', name)
+}
+
 greet('Dmitrii')
+greet2('Dmitrii')
+
 
