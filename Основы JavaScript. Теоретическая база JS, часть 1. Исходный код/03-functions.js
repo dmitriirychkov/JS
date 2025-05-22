9e60ecdf229a6d2c44a0a31e744173a7dd63dc68
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-22 16:20:59 +0500

    Update 03-functions.js
    function greet(name) {
        console.log('Hello - ', name)
    }
    
    const arrow = (name) => {
        console.log('Hello - ', name)
    }
    
    arrow('Dmitrii')

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/03-functions.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/03-functions.js
@@ -1,14 +1,14 @@
 // Function Declaration 
-function greet(name) {
-    console.log('Hello - ', name)
-}
+// function greet(name) {
+//    console.log('Hello - ', name)
+// }
 // Function Expression
-const greet2 = function (name) {
-    console.log('2 Hello - ', name)
-}
+// const greet2 = function (name) {
+//    console.log('2 Hello - ', name)
+// }
 
-greet('Dmitrii')
-greet2('Dmitrii')
+// greet('Dmitrii')
+// greet2('Dmitrii')
 
 // console.dir(greet)
 
@@ -17,15 +17,27 @@ setTimeout(function() {
     greet('Dmitrii2')
 }, 1500)
 */
-
+/*
 let counter = 0
 const interval = setInterval(function() {
- //   if  (counter === 5){
- //       clearInterval(interval)
- //   } else {
+    if  (counter === 5){
+       clearInterval(interval)
+    } else {
        console.log(++counter)
- //   }
-}, 10)
+   }
+}, 1)
+*/
+
+// Arrow Function 
+function greet(name) {
+    console.log('Hello - ', name)
+}
+
+const arrow = (name) => {
+    console.log('Hello - ', name)
+}
+
+arrow('Dmitrii')
 
 
 
