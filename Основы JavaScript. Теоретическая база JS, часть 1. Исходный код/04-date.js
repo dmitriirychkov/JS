28e6868f815e16ab59f4f2a2aded98dc0a021893
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-23 15:09:20 +0500

    Update 04-date.js
    let mode = 'time'
    const output = document.getElementById('output')
    const fullBtn = document.getElementById('full')
    const dateBtn = document.getElementById('date')
    const timeBtn = document.getElementById('time')
    
    output.textContent = new Date()
    
    setInterval(() => {
        output.textContent = format(mode)
    }, 1000)
    
    // Pure Function
    function format(formatMode) {
        const now = new Date()
    
        switch (formatMode) {
            case 'time':
                return now.toLocaleTimeString()
            case 'date':
                return now.toLocaleTimeString()
            case 'full':
                return now.toLocaleTimeString() + ' ' + now.toLocaleString()
            default:
                return now.toLocaleTimeString()
        }
    }

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/04-date.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/04-date.js
@@ -1,9 +1,9 @@
-const now = new Date()
+// const now = new Date()
 
-const start = new Date(1000 * 60 * 60 * 24 * 365)
+// const start = new Date(1000 * 60 * 60 * 24 * 365)
 
 
-const date = new Date(1988, 4, 3, 12, 42, 12)
+// const date = new Date(1988, 4, 3, 12, 42, 12)
 
 // console.log(now.getFullYear())
 // console.log(now.getMonth())
@@ -15,7 +15,37 @@ const date = new Date(1988, 4, 3, 12, 42, 12)
 
 // now.setFullYear(2055)
 
-console.log(now.toDateString())
-console.log(now.toTimeString())
-console.log(now.toLocaleDateString())
-console.log(now.toLocaleTimeString())
+// console.log(now.toDateString())
+// console.log(now.toTimeString())
+// console.log(now.toLocaleDateString())
+// console.log(now.toLocaleTimeString())
+
+//=======
+
+let mode = 'time'
+const output = document.getElementById('output')
+const fullBtn = document.getElementById('full')
+const dateBtn = document.getElementById('date')
+const timeBtn = document.getElementById('time')
+
+output.textContent = new Date()
+
+setInterval(() => {
+    output.textContent = format(mode)
+}, 1000)
+
+// Pure Function
+function format(formatMode) {
+    const now = new Date()
+
+    switch (formatMode) {
+        case 'time':
+            return now.toLocaleTimeString()
+        case 'date':
+            return now.toLocaleTimeString()
+        case 'full':
+            return now.toLocaleTimeString() + ' ' + now.toLocaleString()
+        default:
+            return now.toLocaleTimeString()
+    }
+}
