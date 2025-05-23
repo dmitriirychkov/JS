3d5f5435150444ea90581e0d0b1de0e83cfc8ed7
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-23 15:25:10 +0500

    Update 04-date.js

--- Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/04-date.js
+++ Основы JavaScript. Теоретическая база JS, часть 1. Исходный код/04-date.js
@@ -21,31 +21,44 @@
 // console.log(now.toLocaleTimeString())
 
 //=======
-
-let mode = 'time'
+let mode = 'full'
 const output = document.getElementById('output')
 const fullBtn = document.getElementById('full')
 const dateBtn = document.getElementById('date')
 const timeBtn = document.getElementById('time')
 
-output.textContent = new Date()
+function bindMode(name) {
+  return function () {
+    mode = name
+    update()
+  }
+}
 
-setInterval(() => {
-    output.textContent = format(mode)
-}, 1000)
+fullBtn.onclick = bindMode('full')
+
+dateBtn.onclick = bindMode('date')
+
+timeBtn.onclick = bindMode('time')
+
+setInterval(update, 100)
+update()
+
+function update() {
+  output.textContent = format(mode)
+}
 
 // Pure Function
 function format(formatMode) {
-    const now = new Date()
+  const now = new Date()
 
-    switch (formatMode) {
-        case 'time':
-            return now.toLocaleTimeString()
-        case 'date':
-            return now.toLocaleTimeString()
-        case 'full':
-            return now.toLocaleTimeString() + ' ' + now.toLocaleString()
-        default:
-            return now.toLocaleTimeString()
-    }
+  switch (formatMode) {
+    case 'time':
+      return now.toLocaleTimeString() + '.' + now.getMilliseconds()
+    case 'date':
+      return now.toLocaleDateString()
+    case 'full':
+      return now.toLocaleDateString() + ' ' + now.toLocaleTimeString()
+    default:
+      return now.toLocaleTimeString()
+  }
 }
