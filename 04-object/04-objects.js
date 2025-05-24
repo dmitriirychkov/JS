c026f0f58c72b2886fff6cbbbe1118cba54199e9
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-24 16:01:02 +0500

    Update 04-objects.js
    const person = {
        name: 'Dmitrii',
        age: 37,
        isYoutuber: false,
        languages: ['ru', 'en'],
        address: {
            city: 'Yekaterinburg',
            street: 'Parina'
        },
        'complex key': 'complex',
        [1 + 2]: 'computed value'
    }
    
    console.log(person)

--- 04-object/04-objects.js
+++ 04-object/04-objects.js
@@ -1,2 +1,14 @@
-const person = {}
-console.log(typeof null)
+const person = {
+    name: 'Dmitrii',
+    age: 37,
+    isYoutuber: false,
+    languages: ['ru', 'en'],
+    address: {
+        city: 'Yekaterinburg',
+        street: 'Parina'
+    },
+    'complex key': 'complex',
+    [1 + 2]: 'computed value'
+}
+
+console.log(person)
