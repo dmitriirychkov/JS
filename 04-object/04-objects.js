c529945b9a7f7d9d879167cde6d8d857981d7e31
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-05-29 14:19:14 +0500

    Update 04-objects.js

--- 04-object/04-objects.js
+++ 04-object/04-objects.js
@@ -10,18 +10,17 @@ const person = {
     'complex key': 'complex',
     ['key_' + 21 * 2]: 'computed value',
     greet() {
-        console.log('Greet from person')
+        console.log('Greet from person', this)
     },
     arrow: () => {
         console.log('Person Arrow')
     },
     info() {
-        console.log(this)
         console.log('Person name', this.name)
     }
 }
 
 console.log(person)
-person.info()
+person.greet()
 
 
