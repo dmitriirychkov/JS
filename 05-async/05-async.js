d1fbcc8cbe774b827391b444005ae75eef495779
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-02 13:05:44 +0500

    Update 05-async.js
    function delay(callback, time = 1000) {
       setTimeout(callback, time)
    }
    
    delay(() => {
      console.log('timeout')
    }, 2000)

--- 05-async/05-async.js
+++ 05-async/05-async.js
@@ -10,7 +10,15 @@
 //   console.log('after 3 seconds')
 //   }, 3000)
 
-let count = 0
-setInterval(() => {
-   console.log('tick', ++count)
-}, 1000)
+// let count = 0
+// setInterval(() => {
+//   console.log('tick', ++count)
+// }, 1000)
+
+function delay(callback, time = 1000) {
+   setTimeout(callback, time)
+}
+
+delay(() => {
+  console.log('timeout')
+}, 2000)
