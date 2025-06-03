5fe6f75b7ee02b04895806704b625ebeeca21121
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-03 18:32:10 +0500

    Update app.js

--- 06-result-start/app.js
+++ 06-result-start/app.js
@@ -9,12 +9,25 @@
 // console.log(json)
 // console.log(parsed)
 
+const list = document.querySelector('#list')
+
 async function start() {
     try {
         const resp = await fetch('https://jsonplaceholder.typicode.com/users')
         const data = await resp.json()
-        console.log(data)
+        render(data)
     } catch (err) {} 
 }
+
+function render(users = []) {
+    const html = users.map(toHTML)
+    list.innerHTML = html
+}
+
+function toHTML(user) {
+    return `
+        <li class="list-group-item">${user.name}</li>
+    `
+} 
 
 start()
