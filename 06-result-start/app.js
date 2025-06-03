a7f3bfb0d0e01b5a84319db098cfff73031355d9
dmi <118729000+dmitriirychkov@users.noreply.github.com>
2025-06-03 18:11:28 +0500

    Update app.js
    async function start() {
        try {
            const resp = await fetch('https://jsonplaceholder.typicode.com/users')
            console.log(resp)
        } catch (err) {} 
    }
    
    start()

--- 06-result-start/app.js
+++ 06-result-start/app.js
@@ -1,10 +1,19 @@
-const car = {
-    model: 'Tesla',
-    year: 2025,
+// const car = {
+//    model: 'Tesla',
+//    year: 2025,
+// }
+
+// const json = JSON.stringify(car)
+// const parsed = JSON.parse(json)
+
+// console.log(json)
+// console.log(parsed)
+
+async function start() {
+    try {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+        console.log(resp)
+    } catch (err) {} 
 }
 
-const json = JSON.stringify(car)
-const parsed = JSON.parse(json)
-
-console.log(json)
-console.log(parsed)
+start()
